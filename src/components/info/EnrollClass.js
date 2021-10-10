b17b910546f84d275d6a2f08451a8452b0f195ba
vrojas2 <vrojas2@ucol.mx>
2021-10-10 00:41:22 -0500

    componente reutilizable 'Modal'

--- src/components/info/EnrollClass.js
+++ src/components/info/EnrollClass.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 // import { useForm } from "react-hook-form";
+import Modal from '../../share/Modal';
+// import { SearchOfClass } from './SearchOfClass'
 
 export const EnrollClass = () => {
+    const [active, setActive] = useState(false);
+
     const toggle = () => {
-        console.log("hola perros");
+        setActive(!active);
     }
 
     return (
         <div>
-            <button className="new_class" onClick={toggle}>Nueva Clase</button>
+            <button className="new_class" onClick={toggle}
+            >Nueva Clase</button>
+            <Modal active={active} toggle={toggle}>
+                <h1>Modal Works!</h1>
+                {/* <SearchOfClass></SearchOfClass> */}
+            </Modal>
         </div>
         
     )
