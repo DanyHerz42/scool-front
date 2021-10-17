eb49e05b9d320e5e76ded31a33f78ea8da931f5c
vrojas2 <vrojas2@ucol.mx>
2021-10-17 11:49:14 -0500

    add student

--- src/components/info/Class/IntegrantsStudent.js
+++ src/components/info/Class/IntegrantsStudent.js
@@ -1,14 +1,26 @@
 import React from 'react'
 
+import student1 from '../../../assets/profile/student1.png'
+import student2 from '../../../assets/profile/student2.png'
+
 export const IntegrantsStudent = () => {
 
     return (
         <>
             <div className="containerIntegrant">
                 <div className="containerFlex">
-                    <img className="picIntegrant" src='' alt="fotoAlumno"/>
+                    <img className="picIntegrant" src={student1} alt="fotoAlumno"/>
                     <div className="containerFlexJustCenter">
-                        <p className="name_integrant">Jonathan</p>
+                        <p className="name_integrant">Jonathan Ruiz</p>
+                    </div>
+                </div>
+                <hr/>
+            </div>
+            <div className="containerIntegrant">
+                <div className="containerFlex">
+                    <img className="picIntegrant" src={student2} alt="fotoAlumno"/>
+                    <div className="containerFlexJustCenter">
+                        <p className="name_integrant">Victor Rojas</p>
                     </div>
                 </div>
                 <hr/>
