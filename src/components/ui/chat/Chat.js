03c0c3bac59d235fb6d77db6af6714d441a7b06d
jonathan <yoni_12rrs@hotmail.com>
2021-10-19 02:23:37 -0500

    chat con imgs

--- src/components/ui/chat/Chat.js
+++ src/components/ui/chat/Chat.js
@@ -6,8 +6,20 @@ export const Chat = () => {
     return (
         <div className="chat__container">
             <CommentIcon className="chat__icon-msg"/>
+            <div className="chat__img-profile">
+                <img src="/profile/student1.png" alt="img profile"/>
+            </div>
+            <div className="chat__img-profile">
+                <img src="/profile/student2.png" alt="img profile"/>
+            </div>
             <AccountCircleIcon />
+            <div className="chat__img-profile">
+                <img src="/profile/student3.png" alt="img profile"/>
+            </div>
             <AccountCircleIcon />
+            <div className="chat__img-profile">
+                <img src="/profile/student4.png" alt="img profile"/>
+            </div>
             <AccountCircleIcon />
             <AccountCircleIcon />
             <AccountCircleIcon />
